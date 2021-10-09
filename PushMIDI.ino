9d2c0f6cc13240d0296ae3bca54662f93e7dc259
gjttt1 <dannyherbertgj@gmail.com>
2021-10-09 19:45:37 +0100

    first implimentation

--- PushMIDI.ino
+++ PushMIDI.ino
@@ -1,17 +1,46 @@
 #include <Adafruit_trellis.h>
+#include <MIDIUSB.h>
 
 const byte CHANNEL = 1;
-const byte OFFSET = 9+24;
+const byte OFFSET = 48;
+const byte NUMSWITCHES = 16
 
+Adafruit_Trellis matrix = Adafruit_Trellis();
 
 void setup() 
 {
+    matrix.begin()
 }
 
 void loop()
 {    
+    if(matrix.readSwitches())
+    {
+        for(int i = 0; i < NUMSWITCHES; ++i)
+        {
+            if(matrix.justPressed(i))
+            {
+                setSwitch(i)
+            }
+            if(matrix.justReleased(i))
+            {
+                unsetSwitch(i)
+            }
+        }
+    }
 }
 
+void setSwitch(int switchNum)
+{
+    matrix.setLED(switchNum);
+    sendNoteOn(CHANNEL, switchNum + OFFSET, 127)
+}
+
+void unsetSwitch(int switchNum)
+{
+    matrix.unsetLED(switchNum);
+    sendNoteOff(CHANNEL, switchNum + OFFSET, 127)
+}
 void sendNoteOn(byte channel, byte pitch, byte velocity) 
 {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
